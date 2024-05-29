b6241ab3dc057924c6440d255d71d0dc2cebd127
Jackson <jacksontai.dev@gmail.com>
2024-05-29 11:05:27 +0800

    Add project files.

---
+++ APFood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
