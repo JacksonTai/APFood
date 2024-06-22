68706486923c63a7e6e5dca0fbfeb49b709b814a
Jh <cjuohan47@gmail.com>
2024-06-22 16:23:14 +0800

    Initial setup for login redirect, updated Food column

---
+++ APFood/Controllers/FoodVendorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APFood.Controllers
+{
+    public class FoodVendorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
