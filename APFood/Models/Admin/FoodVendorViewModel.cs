fc5de13c7ed94c55b15b6143c7d03bef534d6328
Jh <cjuohan47@gmail.com>
2024-07-03 00:10:52 +0800

    Admin removed username field

--- APFood/Models/Admin/FoodVendorViewModel.cs
+++ APFood/Models/Admin/FoodVendorViewModel.cs
@@ -11,10 +11,6 @@ namespace APFood.Models.Admin
 
         [Required]
         [EmailAddress]
-        public string UserName { get; set; } = string.Empty;
-
-        [Required]
-        [EmailAddress]
         public string Email { get; set; } = string.Empty;
     }
 }
